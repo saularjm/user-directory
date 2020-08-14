5e7ba06da70950b8a23a6a57817a58dc11997abf
Saular Moaddeli <jake.moaddeli@gmail.com>
2020-08-13 19:09:49 -0700

    started adding components

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header () {
+    return (
+        <div className="header">
+            <h1>Employee Directory</h1>
+            <p>Click on a heading to filter or use the search box to narrow your results.</p>
+        </div>
+    )
+}
+
+export default Header;
--- src/utils/API.js
+++ src/utils/API.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export default{ 
-    getUsers:function(){    
+export default { 
+    search: function() {    
         return axios.get("https://randomuser.me/api/?results=200");
     }
 };
