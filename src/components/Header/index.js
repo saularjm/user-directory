be7144b9fce4050b9713b28d95263eb1fe7ed447
Saular Moaddeli <jake.moaddeli@gmail.com>
2020-08-16 10:15:03 -0700

    added some styling

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,7 +4,9 @@ import "./style.css";
 function Header () {
     return (
         <div className="header">
-            <h1>Friends Directory</h1>
+            <h1 className="headText">Friends Directory</h1>
+            <p>Use the buttons below to filter friends by gender or "Show all" to reset.</p>
+            <p>You can also sort cards alphabetically by name or occupation.</p>
         </div>
     )
 }
--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import friends from "../../friends.json";
 import Card from "../Card";
-
+import "./style.css";
 
 class Main extends Component {
     state = {
@@ -30,12 +30,12 @@ class Main extends Component {
     render() {
         return (
             <div>
-                <button onClick={() => this.filterFriends("Female")}>Show men</button>
-                <button onClick={() => this.filterFriends("Male")}>Show women</button>
-                <button onClick={() => this.resetFriends()}>Show all</button>
+                <button onClick={() => this.filterFriends("Female")} className="btn">Show men</button>
+                <button onClick={() => this.filterFriends("Male")} className="btn">Show women</button>
+                <button onClick={() => this.resetFriends()} className="btn">Show all</button>
                 <br />
-                <button onClick={() => this.sortName()}>Sort by name</button>
-                <button onClick={() => this.sortOcc()}>Sort by occupation</button>
+                <button onClick={() => this.sortName()} className="btn">Sort by name</button>
+                <button onClick={() => this.sortOcc()} className="btn">Sort by occupation</button>
                 {this.state.friends.map(friend => (
                 <Card
                     id={friend.id}
