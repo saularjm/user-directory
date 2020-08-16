5848aa7162af775c28eec475c5f2029ca6f1640a
Saular Moaddeli <jake.moaddeli@gmail.com>
2020-08-16 10:01:31 -0700

    sorting works

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -10,10 +10,20 @@ class Main extends Component {
 
     filterFriends = gender => {
         const friends = this.state.friends.filter(friend => friend.gender !== gender);
-        this.setState({ friends });
+        this.setState({friends});
     }
 
     resetFriends = () => {
+        this.setState({friends});
+    }
+
+    sortName = () => {
+        const friends = this.state.friends.sort((a, b) => a.name.localeCompare(b.name));
+        this.setState({friends});
+    }
+
+    sortOcc = () => {
+        const friends = this.state.friends.sort((a, b) => a.occupation.localeCompare(b.occupation));
         this.setState({friends});
     }
 
@@ -23,6 +33,9 @@ class Main extends Component {
                 <button onClick={() => this.filterFriends("Female")}>Show men</button>
                 <button onClick={() => this.filterFriends("Male")}>Show women</button>
                 <button onClick={() => this.resetFriends()}>Show all</button>
+                <br />
+                <button onClick={() => this.sortName()}>Sort by name</button>
+                <button onClick={() => this.sortOcc()}>Sort by occupation</button>
                 {this.state.friends.map(friend => (
                 <Card
                     id={friend.id}
